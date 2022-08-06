9c3c88597ea3c0156e7b1d62012655fcddd347d3
Ryan Herrington <RyanPHerrington@gmail.com>
2022-08-06 16:07:52 -0400

    Leetcode

--- threeSumClosest.js
+++ threeSumClosest.js
@@ -20,3 +20,46 @@ var threeSumClosest = function(nums, target) {
          }
      }
 }
+
+// Optimized
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var threeSumClosest = function(nums, target) {
+    let closestSum = 9999999;
+    nums.sort((a,b) => a-b);
+	
+    for(let i=0; i<nums.length-2; i++){
+        let j = i+1;
+        let k = nums.length - 1;
+        let closest = 9999999999;
+        while(j<k){
+            let x = nums[i];
+            let y = nums[j];
+            let z = nums[k];
+            
+            let currSum = x+y+z;
+            if(Math.abs(currSum - target) < Math.abs(closest - target)){
+                closest = currSum;
+            }
+            //means increase the current sum
+            if(currSum > target){
+                k--;
+            } else if(currSum < target){//decrease the sum
+                j++;
+            } else {
+                closest = currSum;
+                return closest;
+            }
+        }
+        if(Math.abs(closest - target) < Math.abs(closestSum - target)){
+            closestSum = closest
+        }
+    }
+    
+    return closestSum;
+};
