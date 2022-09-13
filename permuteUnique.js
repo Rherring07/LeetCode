b8e5511112b329a148404b8db27be35aafb4482a
Ryan Herrington <RyanPHerrington@gmail.com>
2022-09-13 17:29:18 -0400

    Leetcode

---
+++ permuteUnique.js
@@ -0,0 +1,23 @@
+// Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ /**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    nums.sort((a,b)=>a-b)
+    let res = []
+
+    let iterate = (arr,temp) =>{
+        if(arr.length == 1){
+            res.push([...temp,arr[0]])
+            return;
+        }
+        for(let i =0;i<arr.length;i++){
+            if(arr[i] == arr[i-1]) continue;
+            iterate(arr.filter((num,idx)=>idx !=i),[...temp,arr[i]])
+        }
+    }
+    iterate(nums,[])
+    return res
+};
