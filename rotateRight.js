e93f526015a20aea21d0c2c757792a6b94eb2b87
Ryan Herrington <RyanPHerrington@gmail.com>
2022-09-25 17:45:02 -0400

    Leetcode

--- rotateRight.js
+++ rotateRight.js
@@ -1,5 +1,18 @@
 // Given the head of a linked list, rotate the list to the right by k places.
-
+/* 
+Algorithm Explanation: (Image attached in comments)
+1.  We count the total number of nodes in the list using count variable,
+2.  Now the total rotations we have to do can be restricted to k= k % count.
+3.  Now we move a pointer from head equal to k positions. This is necessary 
+for the next interesting logic
+4. Now since our ptr is k steps ahead then it will tell us the kth position from end
+i.e we iterate until ptr.next !==null and we move our prev pointer. Once ptr.next===null
+i.e ptr reaches the last node, out prev pointer will be exactly at the kth position from end
+Here kth position from end is nothing but the number of k rotations
+5. Now simply break the list after prev node. Last node i.e ptr will now point to head
+i.e ptr.next=head, next node of prev becomes our new head and finally prev becomes our 
+last node so prev.next=null
+*/
 var rotateRight = function (head, k) {
 	if (!head) return head;
 	let count = 0,
